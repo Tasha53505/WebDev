4ada0b3488ae07e3e6f485091cbc4f0f6b2c2b58
Tasha <ncarr63@gmail.com>
2021-11-11 09:54:47 +1300

    Background Generator Complete
    Complete with a randomizer button.

--- Background Generator/script.js
+++ Background Generator/script.js
@@ -2,25 +2,47 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var button = document.querySelector(".random");
 
-
+//Displays the colour inputs for each sidem, while also displaying the gradient rgb
 function setGradient() {
-    body.style.background = 
-    "linear-gradient(to right, " 
-    +  color1.value
-    + ", " 
-    + color2.value  
-     + ")";
-    
+    body.style.background =
+        "linear-gradient(to right, " +
+        color1.value +
+        ", " +
+        color2.value +
+        ")";
+
+
+    css.textContent = body.style.background + ";";
+
 }
-color1.addEventListener("input", setGradient)
+//Displays the current colours on page load.
+setGradient();
+
+
+//BONUS: Add a random button which generates two random numbers for the colour inputs.
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function randomColors() {
+    color1.value = getRandomColor();
+    color2.value = getRandomColor();
+
+    setGradient();
+}
 
 
 
-color2.addEventListener("input", function(){
-    body.style.background = 
-    "linear-gradient(to right, " 
-    +  color1.value
-    + ", " 
-    + color2.value  
-     + ")";})
+button.addEventListener("click", randomColors, getRandomColor);
+
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
